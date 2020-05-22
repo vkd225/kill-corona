74b148f0c8d86b05b89fb8aa7ce6c71628c4ef15
vkd225 <vkd225@nyu.edu>
2020-05-21 21:01:44 -0400

    adding loaing spinner for leaderboard

--- src/components/leaderboard/Leaderboard.tsx
+++ src/components/leaderboard/Leaderboard.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Table } from 'reactstrap';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Spinner } from 'reactstrap';
 import { ApiService }  from './../../services/ApiService';
 
 interface IProps {
@@ -9,6 +9,7 @@ interface IProps {
 interface IState {
     rankings: any;
     gamesPlayed: number;
+    loading: boolean;
 }
 
 export default class LeaderBoard extends Component<IProps, IState> {
@@ -20,7 +21,8 @@ export default class LeaderBoard extends Component<IProps, IState> {
         this.componentDidMount = this.componentDidMount.bind(this);
         this.state = {
             rankings: [] as any,
-            gamesPlayed: 0
+            gamesPlayed: 0,
+            loading: true
         };
     }
 
@@ -28,83 +30,92 @@ export default class LeaderBoard extends Component<IProps, IState> {
         let data = await this.apiService.getranks()
         this.setState({
             rankings: data.ranks,
-            gamesPlayed: data.gamesPlayed
+            gamesPlayed: data.gamesPlayed,
+            loading: false
         })
     }
 
     render() {
         return (
         <Container fluid style={{ marginTop: 20 }}>
-            <Row  style={{ margin: 10, marginTop: 0 }}>
-                <Col>
-                    <h1>
-                        LEADERBOARD
-                    </h1>
-                </Col>
+            {(this.state.loading) ?
+                <div style={{ textAlign: 'center', marginTop: '25%'}}>
+                    <Spinner color="danger" style={{ width: '5rem', height: '5rem' }} />
+                </div>
+            :
+            <div>
+                <Row  style={{ margin: 10, marginTop: 0 }}>
+                    <Col>
+                        <h1>
+                            LEADERBOARD
+                        </h1>
+                    </Col>
 
-                <Col>
-                    <div style={{ right: 30, position: 'absolute' }}>
-                        <h3 style={{ textTransform: 'capitalize', opacity: 0.9 }}>
-                            Games Played
-                        </h3>
-                        <h3 style={{ right: 0, position: 'absolute' }}>
-                            {this.state.gamesPlayed}
-                        </h3>
-                    </div>
-                </Col>
+                    <Col>
+                        <div style={{ right: 30, position: 'absolute' }}>
+                            <h3 style={{ textTransform: 'capitalize', opacity: 0.9 }}>
+                                Games Played
+                            </h3>
+                            <h3 style={{ right: 0, position: 'absolute' }}>
+                                {this.state.gamesPlayed}
+                            </h3>
+                        </div>
+                    </Col>
 
-            </Row>
-            <Row  style={{ margin: 10, marginTop: 0, marginBottom: 0 }}>
-                <Col>
-                    <Row>
-                        <Col >
-                            <h2>Rank</h2>
-                        </Col>
-                        <Col md='7' lg='7' xl='7'>
-                            <h2>Name</h2>
-                        </Col>
-                        <Col>
-                            <h2 style={{ right: 20, position: 'absolute'}} >Score</h2>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col></Col>
-            </Row>
-            <Row style={{ margin: 10, marginTop: 0 }}>
-                <Col>
-                    <Table dark>
-                        {this.state.rankings.filter(athletes => athletes.rank < 11)
-                            .map((value: any) => {
-                        return (
-                            <tbody>
-                                <tr key={ value.rank } style={{ height: 65 }}>
-                                    <th scope='row' className='h4' style={{ paddingLeft: 20 }}>{value.rank}</th>
-                                    <td className='h4' style={{ left: '21%', position: 'absolute'}}>{value.username}</td>
-                                    <td className='h4' style={{ right: 40, position: 'absolute'}}>{value.score}</td>
-                                </tr>
-                            </tbody>
-                        );
-                        })}
-                    </Table>
-                </Col>
+                </Row>
+                <Row  style={{ margin: 10, marginTop: 0, marginBottom: 0 }}>
+                    <Col>
+                        <Row>
+                            <Col >
+                                <h2>Rank</h2>
+                            </Col>
+                            <Col md='7' lg='7' xl='7'>
+                                <h2>Name</h2>
+                            </Col>
+                            <Col>
+                                <h2 style={{ right: 20, position: 'absolute'}} >Score</h2>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col></Col>
+                </Row>
+                <Row style={{ margin: 10, marginTop: 0 }}>
+                    <Col>
+                        <Table dark>
+                            {this.state.rankings.filter(athletes => athletes.rank < 11)
+                                .map((value: any) => {
+                            return (
+                                <tbody>
+                                    <tr key={ value.rank } style={{ height: 65 }}>
+                                        <th scope='row' className='h4' style={{ paddingLeft: 20 }}>{value.rank}</th>
+                                        <td className='h4' style={{ left: '21%', position: 'absolute'}}>{value.username}</td>
+                                        <td className='h4' style={{ right: 40, position: 'absolute'}}>{value.score}</td>
+                                    </tr>
+                                </tbody>
+                            );
+                            })}
+                        </Table>
+                    </Col>
 
-                <Col>
-                    <Table dark>
-                        {this.state.rankings.filter(athletes => athletes.rank > 10)
-                            .map((value: any) => {
-                        return (
-                            <tbody>
-                                <tr key={ value.rank } style={{ height: 65 }}>
-                                    <th scope='row' className='h4' style={{ paddingLeft: 20 }}>{value.rank}</th>
-                                    <td className='h4' style={{ left: '21%', position: 'absolute'}}>{value.username}</td>
-                                    <td className='h4' style={{ right: 40, position: 'absolute'}}>{value.score}</td>
-                                </tr>
-                            </tbody>
-                        );
-                        })}
-                    </Table>
-                </Col>
-            </Row>
+                    <Col>
+                        <Table dark>
+                            {this.state.rankings.filter(athletes => athletes.rank > 10)
+                                .map((value: any) => {
+                            return (
+                                <tbody>
+                                    <tr key={ value.rank } style={{ height: 65 }}>
+                                        <th scope='row' className='h4' style={{ paddingLeft: 20 }}>{value.rank}</th>
+                                        <td className='h4' style={{ left: '21%', position: 'absolute'}}>{value.username}</td>
+                                        <td className='h4' style={{ right: 40, position: 'absolute'}}>{value.score}</td>
+                                    </tr>
+                                </tbody>
+                            );
+                            })}
+                        </Table>
+                    </Col>
+                </Row>
+            </div>
+            }
         </Container>
         );
     }
