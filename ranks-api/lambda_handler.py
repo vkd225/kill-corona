ed27351fbb6eaa73095b53f2be0e205fc60cecce
vkd225 <vkd225@nyu.edu>
2020-07-01 01:22:09 -0400

    updating api

--- ranks-api/lambda_handler.py
+++ ranks-api/lambda_handler.py
@@ -8,41 +8,68 @@ def add_rank(username, score):
   '''
 
   try:
-    data = send_ranks() # get data from s3 bucket
-
     s3 = boto3.resource('s3')
     bucketname = 'kill-corona'
     itemname = 'ranks.json'
 
+    data = send_ranks() # get data from s3 bucket
+    games = get_all_games() # get all games data
+
+    all_games = games['games']
+    new_game = {'username': username, 'score':score}
+
+    all_games.append(new_game)
+
+    # update all_games file in s3
+    s3object = s3.Object(bucketname, 'all_games.json')
+    s3object.put(
+      Body=(bytes(json.dumps(games).encode('UTF-8')))
+    )
+
     # increment gamesPlayed
     data['gamesPlayed'] = data.get('gamesPlayed', 0) + 1
 
-    # update leaderboard if score is greater
-    for player in data['ranks']:
-      if score >= player.get('score'):
-        player['rank'] = player['rank'] + 1
+    # update leaderboard if score is greater than rank 20
+    last_score = data['ranks'][-1]['score']
 
-    for player in data['ranks']:
-      if score >= player.get('score'):
-        new_rank = player['rank'] - 1
-        data['ranks'].insert(new_rank-1,  {"rank": new_rank, "username": username, "score": score})
-        break
+    if (score > last_score):
+      for player in data['ranks']:
+        if score >= player.get('score'):
+          player['rank'] = player['rank'] + 1
 
-    # remove rank 21
-    data['ranks'].pop()
+      for player in data['ranks']:
+        if score >= player.get('score'):
+          new_rank = player['rank'] - 1
+          data['ranks'].insert(new_rank-1,  {"rank": new_rank, "username": username, "score": score})
+          break
 
-    # write file back to s3 bucket
-    s3object = s3.Object(bucketname, itemname)
-    s3object.put(
-      Body=(bytes(json.dumps(data).encode('UTF-8')))
-    )
+      # remove rank 21
+      data['ranks'].pop()
 
-    return data
+      # write file back to s3 bucket
+      s3object = s3.Object(bucketname, itemname)
+      s3object.put(
+        Body=(bytes(json.dumps(data).encode('UTF-8')))
+      )
+    
+    return 'ranks added'
 
   except Exception as e:
     print (e)
     return 'Error'
 
+def get_all_games():
+  '''
+  get all games data
+  '''
+  s3 = boto3.resource('s3')
+  bucketname = 'kill-corona'
+  itemname = 'all_games.json'
+
+  obj = s3.Object(bucketname, itemname)
+  file_content = obj.get()['Body'].read().decode('utf-8')
+  games = json.loads(file_content)
+  return games
 
 def send_ranks():
   '''
